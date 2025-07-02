74ba00fa3f14a1cbde82cc2353e9a1d153a88202
Akash Adak <161819740+Akash-Adak@users.noreply.github.com>
2025-07-02 10:31:24 +0530

    Time: 1 ms (99.75%), Space: 46.1 MB (57.74%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int i=0;
+      for(int n:nums){
+        if(n!=0) nums[i++]=n;
+      }
+      for(int j=i;j<nums.length;j++){
+        nums[j]=0;
+      }
+    }
+}
